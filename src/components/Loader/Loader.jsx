adb5b5d4896a1dc12905dbd98824be0512e0792c
Aleksandr2905 <natalukha@ukr.net>
2023-10-13 19:41:40 +0300

    style loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,12 @@ import { Hourglass } from 'react-loader-spinner'
 
 export const Loader = () => {
   return (
-    <>
+    <div style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    }}>
       <Hourglass
         visible={true}
         height="80"
@@ -14,6 +19,6 @@ export const Loader = () => {
         wrapperClass=""
         colors={['#306cce', '#72a1ed']}
       />
-    </>
+    </div>
   );
 };
