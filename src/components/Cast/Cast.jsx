0660719894f9670bdfcc6109b95e8a1935e20b22
Aleksandr2905 <natalukha@ukr.net>
2023-10-14 22:37:22 +0300

    add not photo

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchRequestCast } from "components/services/api";
 import css from './Cast.module.css'
+import image from '../image/no-image.png'
 
 const Cast = () => {
     const { movieId } = useParams();
     const [castData, setCastData] = useState(null);
+
 
     useEffect(() => {
         if (!movieId) {
@@ -29,7 +31,9 @@ const Cast = () => {
                 <ul className={css.cardCast}>
                     {castData?.cast?.map(cast => (
                         <li className={css.castItem} key={cast.id}>
-                            <img className={css.castImg} src={`https://image.tmdb.org/t/p/w185${cast.profile_path}`} alt={cast.name} />
+                            <img className={css.castImg} src={`https://image.tmdb.org/t/p/w185${cast.profile_path}` || { image }} alt={cast.name} onError={(e) => {
+                                e.target.src = image;
+                            }} />
                             <p className={css.castText}>{cast.name}</p>
                         </li>
                     ))}
